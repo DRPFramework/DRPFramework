5bc3db445f6de9f053660d0aecb8cb456acf2d57
Baran ÖZCAN <drawwbeta@gmail.com>
2020-09-26 21:31:17 +0300

    fix priority load object

--- src/server/Main.ts
+++ src/server/Main.ts
@@ -7,9 +7,6 @@ import { DatabaseManager } from './database/DatabaseManager';
 import { BanManager } from './manager/BanManager';
 import { GroupManager } from './manager/GroupManager';
 
-import './extensions/Player';
-import './systems/AuthSystem';
-
 const mainDir = alt.getResourcePath('drp');
 const configDir = path.join(mainDir, 'config');
 
@@ -24,6 +21,9 @@ type ConfigType = {
 
 const config: ConfigType = JSON.parse(fs.readFileSync(path.join(configDir, 'general.json'), 'utf-8'));
 
+const banManager: BanManager = new BanManager();
+const groupManager: GroupManager = new GroupManager();
+
 const databaseManager: DatabaseManager = new DatabaseManager({
   host: config.database.host,
   database: config.database.database,
@@ -36,10 +36,13 @@ databaseManager.open((_connection, error) => {
     alt.logError(error);
     process.exit(1);
   }
-  alt.log('Database connection established.');
+  alt.log('[DRP] Database connection established.');
 });
 
-const banManager: BanManager = new BanManager();
-const groupManager: GroupManager = new GroupManager();
+import './extensions/Player';
+
+import './events/PlayerConnect';
+
+import './systems/AuthSystem';
 
 export { mainDir, configDir, config, databaseManager, banManager, groupManager };
